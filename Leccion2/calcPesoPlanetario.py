b2ce35be7e80d28869a2617e9a7023d7018bb9fa
jhuset2003 <jhusetqp@gmail.com>
2022-03-19 09:16:28 -0500

    Medio finalizado

--- Leccion2/calcPesoPlanetario.py
+++ Leccion2/calcPesoPlanetario.py
@@ -1,6 +1,7 @@
 #Albert Quintanilla y James Cadena
 
 from ast import Try
+from cgitb import text
 from tkinter import *
 
 OptionList = [
@@ -14,14 +15,13 @@ OptionList = [
 "Neptuno"
 ] 
 
-
-def calcular_masa():
-    equiation.set(int (equiation.get())/ 9.8)
-
-
+def calcular_peso_planetario():
+    if equiation2 == equiation2[0]:
+        resultado_peso.set((float(equiation.get)*3.7)/9.8)
+        print(resultado_peso)
 
 calcular_peso = Tk()
-calcular_peso.title("Calcula tu peso")
+calcular_peso.title("Calcula tu peso en otros planetas")
 
 frame = Frame(calcular_peso, pady=3,padx=12)
 frame.grid(column=0,row=0)
@@ -29,27 +29,23 @@ frame.grid(column=0,row=0)
 frame2 = Frame(calcular_peso, pady=3,padx=12)
 frame2.grid(column=1,row=0 )
 
-equiation = StringVar()
+equiation = IntVar()
 alc_input = Entry(frame2, text=equiation).grid(column=1,row=0)
 
-Button(frame2,width=10, text="Calcular",fg="#fff", background="#666", command= calcular_masa).grid(column=1,row=2)
 
- 
-
-equiation2 = StringVar()
-
-
-
+equiation2 = IntVar()
 equiation2.set(OptionList[0])
 
-opt = OptionMenu(frame, equiation2, *OptionList)
+
+opt = OptionMenu(frame, equiation2, * OptionList)
 opt.grid(column=3,row=0)
-opt.config(width=10, font=('Helvetica', 12))
+
+resultado_peso = IntVar()
+res = Label(frame2, textvariable=resultado_peso).grid(column=1,row=3)
 
 
 
-
-
+Button(frame2,width=10, text="Calcular",fg="White", background="#666", command=calcular_peso_planetario).grid(column=1,row=2)
 
 
 calcular_peso.mainloop()
